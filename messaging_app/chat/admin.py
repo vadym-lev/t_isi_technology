76fd37bdf7f83a98a9cb42dbe73c8104e3b58a89
vadym <vadlevkovskiy@gmail.com>
2024-09-18 19:02:46 +0300

    update test data

--- messaging_app/chat/admin.py
+++ messaging_app/chat/admin.py
@@ -1,13 +1,33 @@
 from django.contrib import admin
 from .models import Thread, Message
 
+
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 1
+    readonly_fields = ('created', 'sender')
+
+
+@admin.register(Thread)
 class ThreadAdmin(admin.ModelAdmin):
-    list_display = ['id', 'created', 'updated']
-    search_fields = ['id']
+    list_display = ('id', 'get_participants', 'created', 'updated')
+    list_filter = ('created', 'updated')
+    search_fields = ('participants__username',)
+    inlines = [MessageInline]
 
+    def get_participants(self, obj):
+        return ", ".join([user.username for user in obj.participants.all()])
+    get_participants.short_description = 'Participants'
+
+
+@admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'sender', 'text', 'created', 'is_read']
-    search_fields = ['text', 'sender__username']
+    list_display = ('id', 'thread', 'sender', 'text', 'is_read', 'created')
+    list_filter = ('is_read', 'created')
+    search_fields = ('sender__username', 'text')
+    list_editable = ('is_read',)
+    actions = ['mark_as_read']
 
-admin.site.register(Thread, ThreadAdmin)
-admin.site.register(Message, MessageAdmin)
+    def mark_as_read(self, request, queryset):
+        queryset.update(is_read=True)
+    mark_as_read.short_description = 'Mark selected messages as read'
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -32,6 +33,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('chat/', include('chat.urls')),
